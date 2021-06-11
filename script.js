52028a26679cefdeff2015310c29da2e56fc2b98
codingCat-dev <85181534+codingCat-dev@users.noreply.github.com>
2021-06-11 22:19:38 +0200

    Add files via upload
    Final website for my fictional Gym landing page.
    
    On second project of Guardian Gym i was primarly focused on code refactoring, some simple functionality like using random.user API, onScroll behavior and a better design(hopefully) :P
    
    Used html,javascript and css compiled in sass. randomuser API, leaflet.
    
    leaflet functionality is not working on smaller screen sizes, had some issues with map loading.

---
+++ script.js
@@ -0,0 +1,118 @@
+'use strict';
+
+const loadingScreen = document.querySelector('.loading-screen');
+const navigationLogo = document.querySelector('.navigation-logo');
+
+const dataCounters = document.querySelectorAll('.counter-content');
+
+dataCounters.forEach(counter => {
+  counter.innerText = '0';
+  // counter function
+  const counterUpdate = () => {
+    const dataTarget = +counter.getAttribute('data-target');
+    const data = +counter.innerText;
+    // counter function
+    const counterIncrement = dataTarget / 750;
+
+    if (data < dataTarget) {
+      counter.innerText = `${Math.floor(data + counterIncrement)}`;
+      setTimeout(counterUpdate, 1);
+    } else {
+      counter.innerText = dataTarget;
+    }
+  };
+  // counter function
+  counterUpdate();
+}); // counter function
+
+// Form
+const formHolder = document.querySelector('.form-holder');
+const formModalBox = document.querySelector('.form-modal');
+
+formHolder.addEventListener('submit', e => {
+  e.preventDefault();
+  const input = e.target;
+  if (input.value === '') {
+    return;
+  } else {
+    setTimeout(() => {
+      formModalBox.classList.remove('hidden');
+      formModalBox.classList.add('formThanksAnimation');
+    }, 1000);
+    setTimeout(() => {
+      formHolder.classList.add('hidden');
+    }, 900);
+  }
+});
+
+const reviewsList = document.querySelector('.reviews-list');
+const reviewsButton = document.querySelector('.reviews-button');
+
+const getData = async () => {
+  const res = await fetch('https://randomuser.me/api?results=14');
+  const { results } = await res.json();
+
+  results.forEach(user => {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+    <img class="reviews-image" src="${user.picture.large}" alt="${user.name.first}">
+    <div class="reviews-info">
+    <span>${user.name.first} <br> ${user.name.last}</span>
+
+    </div>`;
+
+    reviewsList.appendChild(div);
+  });
+};
+
+getData();
+
+const map = L.map('map').setView([50.0578399, 14.4316543], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 18,
+  minZoom: 14,
+  id: 'mapbox/streets-v11',
+}).addTo(map);
+
+const gymIcon = L.icon({
+  iconUrl: 'img/logomap.png',
+
+  iconSize: [40, 40],
+
+  iconAnchor: [20, 100],
+
+  popupAnchor: [0, -100],
+});
+
+L.marker([50.0578399, 14.4316543], { icon: gymIcon })
+  .addTo(map)
+  .bindPopup('Guardian Gym')
+  .openPopup();
+
+// // sections reveal
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section-hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section-hidden');
+});
